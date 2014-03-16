f46a4f9ea976dd597068c31fff49f8ebabd7f24d
Louis Brutton <louisbrutton@gmail.com>
2014-03-16 20:24:23 +0100

    Finish user signup

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -4,8 +4,58 @@ describe "UserPages" do
 	subject{page}
 
 	describe "signup page" do
+
 		before {visit signup_path}
+
 		it {should have_content('Sign up')}
 		it {should have_title(full_title('Sign Up'))}
+
+		let(:submit) { "Create my account"}
+
+		describe "when the information is invalid" do
+			it "should not create a user" do
+				expect{ click_button submit }.not_to change(User, :count)
+			end
+		end
+
+		describe "when the information is valid" do
+
+			before do
+				fill_in "Name", with:"Example"
+				fill_in "Email", with:"example@hotmail.com"
+				fill_in "Password", with:"foobar"
+				fill_in "Confirmation", with:"foobar"
+			end
+
+			it "should create a user" do
+				expect{ click_button submit}.to change(User, :count).by(1)
+			end
+		end
+
+		describe "the signup page after a failed signup" do
+
+			before do
+				fill_in "Name", with: "Example"
+				fill_in "Email", with: "somethingwrong"
+				click_button submit
+			end
+
+			it { should have_content('Email is invalid')}
+
+		end
+
+
   	end
+
+  	describe "profile page" do
+
+		let(:user) { FactoryGirl.create(:user) }
+
+		before { visit user_path(user)}
+
+		it {should have_title(user.name)}
+		it {should have_content(user.name)}
+	end
 end
+
+
